05076d517bb5469341e1c2ef6b14fc2bf775b3fd
jason <jason@noisybox.net>
2020-11-27 12:45:05 -0800

    first usage data

---
+++ static/chart-opensignal.js
@@ -0,0 +1,54 @@
+
+function drawAllOpenSignal(){
+  fetch('./data/openSignal.json')
+    .then(response => response.json())
+    .then(data => {
+      drawOpenSignalHandsetMenu(data);
+    });
+}
+
+function drawOpenSignalHandsetMenu(data){
+  const pickups = buildDateSeries(data.handsetPickups);
+  const handsetPickupData = Object.entries(pickups)
+    .map(entry => entry[1]);
+  const labels = Object.entries(pickups)
+      .map(entry => entry[0]);
+
+  const datasets = [{
+      label: "OpenSignal handset pickups",
+      borderColor: stringToColor("OpenSignal handset pickups"),
+      data: handsetPickupData
+  }];
+  console.log(datasets);
+
+  const ctx = document.getElementById('os-handset-menu').getContext('2d');
+  charts.opensignalHandsetMenuChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: labels,
+        datasets: [{
+          label: 'OpenSignal handset pickups',
+          borderColor: stringToColor("OpenSignal handset pickups."),
+          data: handsetPickupData,
+        }],
+      },
+      options: {
+        responsive: true
+      }
+  });
+}
+
+// ensures the data has consistent date range (no date gaps)
+function buildDateSeries(data){
+  const first = new Date(Object.entries(data)[0][0]);
+  const last = new Date(Object.entries(data).slice(-1)[0][0]);
+  console.log(`${first} and ${last}`)
+  const d = new Date(first);
+  const result = [];
+  while(d <= last){
+    result.push(formatDate(d));
+    d.setDate(d.getDate() + 1);
+  }
+  const entries = result.map(d => [d, data[d] || 0]);
+  return Object.fromEntries(entries);
+}
