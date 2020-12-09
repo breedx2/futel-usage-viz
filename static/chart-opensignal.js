a9bfbcd753bfc4ab35e96b12fd6f08325ee7cc9c
jason <jason@noisybox.net>
2020-12-08 21:18:40 -0800

    rollup opensignal by month

--- static/chart-opensignal.js
+++ static/chart-opensignal.js
@@ -2,10 +2,11 @@ function drawAllOpenSignal() {
   fetch('./data/openSignal.json')
     .then(response => response.json())
     .then(data => {
-      drawOpenSignalHandsetMenu(data);
-      drawOpenSignalRemoteMenu(data);
-      drawOpenSignalHandsetContent(data);
-      drawOpenSignalRemoteContent(data);
+      const monthData = rollupOpenSignalToMonths(data);
+      drawOpenSignalHandsetMenu(monthData);
+      drawOpenSignalRemoteMenu(monthData);
+      drawOpenSignalHandsetContent(monthData);
+      drawOpenSignalRemoteContent(monthData);
     });
 }
 
@@ -30,7 +31,7 @@ function drawOpenSignalRemoteMenu(data) {
 }
 
 function drawOpenSignalMenu(opts) {
-  const series = buildDateSeries(opts.data);
+  const series = openSignalMonthRangeFromObj(opts.data);
   const labels = Object.keys(series);
   const data = Object.values(series);
 
@@ -68,10 +69,9 @@ function drawOpenSignalRemoteContent(data) {
 }
 
 function drawOpenSignalContent(opts) {
-  const [start, end] = dateRangeMultiSeries(opts.multiSeries);
-  const peoplesHomesData = buildDateSeriesRange(opts.multiSeries.peoples_homes, start, end);
-  const conversationsData = buildDateSeriesRange(opts.multiSeries.conversations, start, end);
-  const missedConnData = buildDateSeriesRange(opts.multiSeries.missed_connections, start, end);
+  const peoplesHomesData = openSignalMonthRangeFromObj(opts.multiSeries.peoples_homes);
+  const conversationsData = openSignalMonthRangeFromObj(opts.multiSeries.conversations);
+  const missedConnData = openSignalMonthRangeFromObj(opts.multiSeries.missed_connections);
 
   const datasets = [
     {
@@ -104,6 +104,56 @@ function drawOpenSignalContent(opts) {
   });
 }
 
+function rollupOpenSignalToMonths(data){
+  const allMonths = openSignalMonthRange(data);
+
+  return {
+    handsetPickups: rollupMonths(allMonths, data.handsetPickups),
+    remoteMenu: rollupMonths(allMonths, data.remoteMenu),
+    handsetContent: Object.fromEntries(
+      Object.entries(data.handsetContent).map(e => [e[0], rollupMonths(allMonths, e[1])])
+    ),
+    remoteContent: Object.fromEntries(
+      Object.entries(data.remoteContent).map(e => [e[0], rollupMonths(allMonths, e[1])])
+    ),
+  };
+}
+
+function openSignalMonthRange(data){
+  const months = new Set();
+  addUniqueMonths(data.handsetPickups, months);
+  addUniqueMonths(data.remoteMenu, months);
+  Object.values(data.handsetContent).forEach(obj => addUniqueMonths(obj, months));
+  Object.values(data.remoteContent).forEach(obj => addUniqueMonths(obj, months));
+  return openSignalMonthRangeFromSet(months);
+}
+
+function openSignalMonthRangeFromObj(obj){
+  const months = new Set();
+  Object.keys(obj).forEach(m => months.add(m));
+  return rollupMonths(openSignalMonthRangeFromSet(months), obj, x=>x);
+}
+
+function openSignalMonthRangeFromSet(months){
+  const sorted = [...months].sort();
+  const first = sorted[0];
+  const last = sorted.slice(-1)[0];
+  return buildContinuousMonthRange(first, last);
+}
+
+function addUniqueMonths(obj, set){
+  Object.keys(obj).map(d => d.replace(/-\d\d$/, '')).forEach(d => set.add(d));
+}
+
+function rollupMonths(allMonths, obj, dateToMonth = d => d.replace(/-\d\d$/, '')){
+  const result = Object.fromEntries(allMonths.map(m => [m,0]));
+  return Object.entries(obj).reduce((acc,e) => {
+    const month = dateToMonth(e[0]);
+    result[month] += e[1];
+    return result;
+  }, result);
+}
+
 function dateRangeMultiSeries(data) {
   const all = Object.values(data);
   const allBounds = all.flatMap(series => {
@@ -112,17 +162,4 @@ function dateRangeMultiSeries(data) {
   });
   const sorted = allBounds.sort();
   return [new Date(sorted[0]), new Date(sorted.slice(-1)[0])];
-}
-
-// ensures the data has consistent date range (no date gaps)
-function buildDateSeries(data) {
-  const first = new Date(Object.keys(data)[0]);
-  const last = new Date(Object.keys(data).slice(-1)[0]);
-  return buildDateSeriesRange(data, first, last);
-}
-
-function buildDateSeriesRange(data, first, last) {
-  const result = buildDateRange(first, last);
-  const entries = result.map(d => [d, data[d] || 0]);
-  return Object.fromEntries(entries);
 }
