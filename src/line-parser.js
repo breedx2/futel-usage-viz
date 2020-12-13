6a9d80e64b8e206292142f170f9f0b8e53e9172c
jason <jason@noisybox.net>
2020-12-13 14:05:34 -0800

    add extension parser

--- src/line-parser.js
+++ src/line-parser.js
@@ -6,12 +6,14 @@ class LineParser {
     const re = /(\d\d\d\d-\d\d-\d\d) (\d\d):.* CALLERID\(number\)=\+?\w*, UNIQUEID=(.*), CHANNEL=(.*), name=(.*)/;
     const [x, date, hour, uid, channel, name] = line.match(re);
     const [xx, timestamp] = line.match(/^(.*) CALLERID.*/);
+    const [xxx, extension] = channel.match(/(SIP.*)-.*/);
     return {
       timestamp: timestamp,
       date: date,
       hour: parseInt(hour),
       uid: uid,
       channel: channel,
+      extension: extension,
       name: name
     }
   }
